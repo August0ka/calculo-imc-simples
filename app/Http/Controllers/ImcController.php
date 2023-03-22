8eaeff53d8e623e5c2b16ce476368d3fb86f6906
August0ka <kiichi_sassagima@outlook.com>
2023-03-22 07:36:40 -0400

    AtualizaçãoV1

--- app/Http/Controllers/ImcController.php
+++ app/Http/Controllers/ImcController.php
@@ -7,28 +7,32 @@ use Illuminate\Http\Request;
 class ImcController extends Controller
 {
     public function calculoImc(Request $request) {
+        
         $dados = [
             'nome' => $request -> input('Nome'),
             'peso' => $request -> input('Peso'),
             'altura' => $request -> input('Altura')
         ];
 
-        $imc = $dados['peso'] / ($dados['altura'] * $dados['altura']);
+        if($dados['peso'] > 0) {
 
-        $resultadoImc = "";
+            $imc = $dados['peso'] / ($dados['altura'] * $dados['altura']);
 
-        if($imc < 18.5) {
+            $resultadoImc = " ";
+
+            if($imc < 18.5) {
             $resultadoImc = "Magreza";
-        } elseif($imc >= 18.5 && $imc <= 24.9) {
-            $resultadoImc = "Normal";
-        } elseif($imc >= 25.0 && $imc <= 29.9) {
-            $resultadoImc = "Sobrepeso";
-        } elseif($imc >= 30.0 && $imc <= 39.9) {
-            $resultadoImc = "Obesidade";
-        } elseif($imc > 40.0) {
-            $resultadoImc = "Obesidade grave";
+            } elseif($imc >= 18.5 && $imc <= 24.9) {
+                $resultadoImc = "Normal";
+            } elseif($imc >= 25.0 && $imc <= 29.9) {
+                $resultadoImc = "Sobrepeso";
+            } elseif($imc >= 30.0 && $imc <= 39.9) {
+                $resultadoImc = "Obesidade";
+            } elseif($imc > 40.0) {
+                $resultadoImc = "Obesidade grave";
+            }
         }
-        echo $resultadoImc;
-        return view('calculaImc', $dados);
+
+        return view('calculaImc', $dados, ['resultadoImc' => $resultadoImc]);
     }
 }
--- resources/views/calculaImc.blade.php
+++ resources/views/calculaImc.blade.php
@@ -5,28 +5,77 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Calcula Imc</title>
+    <style>
+        header {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            padding: 20px;
+            background-color: limegreen;
+            font-family: Cambria, Arial;
+        }
+
+        section{
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 80vh;
+            font-family: Cambria, Arial;
+            color: black;
+        }
+
+        form{
+            font-family: Arial;
+        }
+
+        form div{
+            display: flex;
+            flex-direction: colune;
+            margin-bottom: 15px;
+        }
+
+        form input, form textarea{
+            outline: unset;
+            padding: 20px;
+            width: 100px;
+            border: 1px solid forestgreen;
+            border-radius: 20px;
+        }
+
+        h2{
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            padding: 20px;
+            background-color: limegreen;
+            font-family: Cambria, Arial;
+        }
+
+    </style>
 </head>
 <body>
     <header>
         <h1>Calculadora de imc</h1>
     </header>
     <section>
-        <hr>
         <form method="get">
-
-            <b>Nome</b>
-            <input type="text" name="Nome"> <br><br>
-
-            <b>Peso</b>
-            <input type="text" name="Peso"> <br><br>
-
-            <b>Altura</b>
-            <input type="text" name="Altura"> <br><br>
-
-            <input type="submit" value="Enviar">
-            
+            <div>
+                <b>Nome</b>
+                <input type="text" name="Nome"> <br><br>
+            </div>
+            <div>
+                <b>Peso</b>
+                <input type="text" name="Peso"> <br><br>    
+            </div>
+            <div>
+                <b>Altura</b>
+                <input type="text" name="Altura"> <br><br>                
+            </div>
+            <div>
+                <input type="submit" value="Enviar">
+            </div> 
         </form>
-        <hr>
     </section>
+    <h2>De acordo com seu imc você se encaixa na categoria {{$resultadoImc}}</h2>
 </body>
 </html>
